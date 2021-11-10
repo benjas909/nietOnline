9446efc1e929e20708b1bd806494f297d440206b
benjas909 <benjamin.aguilerac@usm.cl>
2021-11-10 16:36:22 -0300

    Agrega pagina de info

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.inicio, name="page-home")]
+urlpatterns = [
+    path("", views.inicio, name="page-home"),
+    path("info/", views.about, name="info-pagina"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def inicio(request):
-    return HttpResponse('<h1>El benja es puto</h.1>')
 
+def inicio(request):
+    return HttpResponse("<h1>chupalo zamquin buenas tardes</h1>")
+
+
+def about(request):
+    return HttpResponse("<h1>Sobre los pibes</h1>")
