631fdfe269e60b549d6377be389515748a459600
KuVe076 <gonzalo.alarconc@usm.cl>
2021-11-13 15:42:22 -0300

    about us date eliminated

--- blog/views.py
+++ blog/views.py
@@ -8,13 +8,11 @@ posts = [
         "titulo": "Bienvenidos a NietOnline!",
         "autor": "NERV1_DEV",
         "contenido": "Esta página busca ayudar a personas de tercera edad a aprender a usar las aplicaciones modernas.",
-        "fecha": "7/11/2021",
     },
     {
         "titulo": "¿Quiénes Conforman el grupo?",
         "autor": "NERV1_DEV",
         "contenido": "El grupo está conformado por Alexander Inostroza, Felipe Zambrano, Gonzalo Alarcón y Benjamín Aguilera.",
-        "fecha": "8/11/2021",
     },
 ]
 
