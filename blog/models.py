e3ad06f7809fd6ed1f9dce77a8cc5e0e2900c48d
Alexander Inostroza <elAlex1111@hotmail.cl>
2021-11-14 21:48:46 -0300

    Tag and Tutorial class added to models

--- blog/models.py
+++ blog/models.py
@@ -6,4 +6,14 @@ class Iconos(models.Model):
     def __str__(self):
         return self.name
 
+class Tutorial(models.Model):
+    link = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
+    def __str__(self):
+        return self.link
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    tutorials = models.ManyToManyField(Tutorial)
+    def __str__(self):
+        return self.name
